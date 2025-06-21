849bd1c151e64985e3647fc96389568ac36f3dc5
ha-ccoon <codeminhasohn@gmail.com>
2025-06-21 17:42:16 +0900

    chore: add adversarial noise agent to root agent

--- agents/copyright/agent.py
+++ agents/copyright/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents import Agent
 
 from .prompts import return_instructions_root
-from .sub_agents import watermark_agent
+from .sub_agents import watermark_agent, adversarial_noise_agent
 
 
 
@@ -14,7 +14,7 @@ root_agent = Agent(
   name="copyright_multiagent",
   global_instruction="You are a Artwork Copyright Protection Multi Agent System.",
   instruction=return_instructions_root(),
-  sub_agents=[watermark_agent]
+  sub_agents=[watermark_agent, adversarial_noise_agent]
 )
 
 
