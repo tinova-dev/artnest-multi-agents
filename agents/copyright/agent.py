0cf752e212adb4ea4ffb496f57b12c6d3bd00e52
ha-ccoon <codeminhasohn@gmail.com>
2025-06-21 14:01:20 +0900

    chore: add copyright multi agents

---
+++ agents/copyright/agent.py
@@ -0,0 +1,22 @@
+from google.adk.agents import Agent
+
+from .prompts import return_instructions_root
+from .sub_agents import watermark_agent
+
+
+
+MODEL = 'gemini-1.5-flash-002'
+ORIGINAL_PATH = './data/original'
+PROTECTED_PATH = './data/protected'
+
+root_agent = Agent(
+  model=MODEL,
+  name="copyright_multiagent",
+  global_instruction="You are a Artwork Copyright Protection Multi Agent System.",
+  instruction=return_instructions_root(),
+  sub_agents=[watermark_agent]
+)
+
+
+
+
---
+++ agents/copyright/sub_agents/watermark/agent.py
@@ -0,0 +1,32 @@
+import json
+import os
+from dotenv import load_dotenv
+
+from google.adk.tools import ToolContext
+from google.adk.agents import Agent
+
+from .prompts import return_instructions_watermark
+from .tools import encode_image, save_state
+
+load_dotenv()
+
+
+root_agent = Agent(
+    model=os.getenv("MODEL"),
+    name="watermark_agent",
+    description="Applies an invisible watermark to an artwork image using robust encoding methods. The watermark includes copyright metadata in JSON format to protect the creator’s rights.",
+    instruction=return_instructions_watermark(),
+    tools=[encode_image]
+)
+
+# import asyncio
+# if __name__ == "__main__": # Ensures this runs only when script is executed directly
+#     print("Executing using 'asyncio.run()' (for standard Python scripts)...")
+#     try:
+#         # This creates an event loop, runs your async function, and closes the loop.
+#         asyncio.run(root_agent())
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+
+
